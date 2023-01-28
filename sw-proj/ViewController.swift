89542ac7b317605fd6a744ddeb558e95203e2e77
Zorana-jvc <zorana.jevtic@stud.fh-campuswien.ac.at>
2023-01-28 18:19:12 +0100

    Initial Commit

---
+++ sw-proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sw-proj
+//
+//  Created by Zorana Jevtic on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
